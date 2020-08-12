ee79ee7e99bcda4e57366e451d6510e707d1f977
R2 <lethe@R2.attlocal.net>
2020-08-12 14:59:51 -0500

    setting up our react app

---
+++ src/config.js
@@ -0,0 +1,17 @@
+export default {
+  s3: {
+    REGION: "us-east-2",
+    BUCKET: "notes-app-uploads-jeffgojeff"
+  },
+  apiGateway: {
+    REGION: "us-east-2",
+    URL: "https://60apf2qeqe.execute-api.us-east-2.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_7xWxjzKvU",
+    APP_CLIENT_ID: "71enjreup438hrrhbd26r24c37",
+    IDENTITY_POOL_ID: "us-east-2:5aabb612-e4b7-4c86-a2c6-0d7e58d2584f"
+  }
+};
+
